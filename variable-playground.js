3793b07c2111f0b3dcbe78887cbac42dd8b98cf7
Roman Gitzis <romang82@gmail.com>
2020-01-24 17:51:08 +0200

    add variable playground area

---
+++ variable-playground.js
@@ -0,0 +1,23 @@
+//var person = {
+//		name: 'andrew',
+//		age:21
+//};
+//
+//function updatePerson (obj) {
+//	//obj = {
+ //	//name: 'andrew',
+	//age: 24
+	//};
+	//obj.age = 24;
+//}
+
+//updatePerson(person);
+//console.log(person);
+
+var people = ["yoni", "Dan"];
+//updatePerson(person);
+
+people.push('andrew');
+debugger;
+console.log(people);
+
