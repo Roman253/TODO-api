db1ffefca60c32d83d13c79540bd08f14f9aa538
Roman Gitzis <romang82@gmail.com>
2020-01-31 00:51:26 +0200

    add postgres for proudction2

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 var Sequelize = require('sequelize');
-var env = process.env.NODE_ENV || 'developemnt';
+var env = process.env.NODE_ENV || 'developement';
 var sequelize;
 
 if (env === 'production'){
