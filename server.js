bd45d90d94b87149c922871542576ae66922f44c
Roman Gitzis <romang82@gmail.com>
2020-02-01 17:42:00 +0200

    hash and salt passowrd

--- server.js
+++ server.js
@@ -119,7 +119,7 @@ app.post('/users', function(req,res) {
 	var body = _.pick(req.body, 'email', 'password');
 
 	db.user.create(body).then(function(user) {
-			res.json(user.toJSON());
+			res.json(user.toPublicJSON());
 		}, function(e){
 			res.status(400).json(e);
 	});
@@ -127,7 +127,7 @@ app.post('/users', function(req,res) {
 
 
 
-db.sequelize.sync().then(function(){
+db.sequelize.sync({force: true}).then(function(){
 	app.listen(PORT, function() {
 		console.log('Express listening on port ' + PORT + '!');
 	});	
